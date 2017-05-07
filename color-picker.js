7d043051471c7552ccf0af2eaf8133e5dbdebf16
Christopher-Liu <christopherbrianliu@gmail.com>
2017-05-07 12:58:24 -0700

    Creating the Repo
    - Creating the repo for the color picking game
    - Implemented basic logic for generating the random colors of the boxes
    - Implemented basic responsive grid system for the boxes themselves

---
+++ color-picker.js
@@ -0,0 +1,29 @@
+var rgbTextDisplay = document.querySelector("#colorToGuess");
+var colorBoxes = document.querySelectorAll(".gridItem");
+
+
+/* Function for generating the random color */
+function getRandomHexColor(){
+  var values = "0123456789ABCDEF";
+  var hexColor = "#";
+
+  for (var i=0; i<6; i++){
+    hexColor += values[Math.floor(Math.random()*16)];
+  }
+
+  return hexColor;
+}
+
+
+
+/* Selecting one of the color values to be the value to guess */
+var correctColorIndex = Math.floor(Math.random()*6);
+
+/* Giving all of the boxes a random color */
+for (var i=0; i<colorBoxes.length; i++){
+  colorBoxes[i].style.backgroundColor = getRandomHexColor();
+
+  if (i == correctColorIndex) {
+    rgbTextDisplay.textContent = colorBoxes[i].style.backgroundColor;
+  }
+}
